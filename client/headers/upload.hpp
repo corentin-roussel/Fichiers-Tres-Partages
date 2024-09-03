2648ec7d99a37c0dd4e7fbe9538f7614c88f47d3
corentin-roussel <corentin.roussel@laplateforme.io>
2024-09-03 17:25:11 +0200

    change getFileSize parameter add a chunkSize parameter and start upload files by setting parameters

--- client/headers/upload.hpp
+++ client/headers/upload.hpp
@@ -5,13 +5,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <limits.h>
-#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <filesystem>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#define __min(a,b)  (((a) < (b)) ? (a) : (b))
 
 namespace fs = std::filesystem;
 
@@ -20,7 +22,9 @@ private:
     char* filename_;
     struct stat sb_;
     char buffer_[1024];
+    char *pathToFile;
     const char* fileToCreate_ = "files/";
+    int chunkSize_ = 4*1024;
 
 public:
     Upload();
@@ -28,10 +32,10 @@ public:
     ~Upload();
     Upload& operator=(const Upload &other);
 
-    void uploadFile();
+    int uploadFile(ssize_t fileSize, char *buffer, int chunkSize, int fileDescriptor);
     void downloadFile();
     void createDirectory();
-    int getFileSize(const char* filename);
+    ssize_t getFileSize(const char* filename);
     char *getExePath();
     fs::path getExePath(char *buffer);
 
@@ -43,4 +47,5 @@ public:
     void setBuffer(char buffer[]);
     const char* getFileToCreate();
     void setFileToCreate(char * fileName);
+    int getChunkSize();
 };
